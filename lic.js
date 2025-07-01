915a715194acf7a8ead2d8ada67f5cefd83ec1a7
kaldybaev312 <ilimkaldybaev5@gmail.com>
2025-07-01 14:09:53 +0600

    asd

---
+++ lic.js
@@ -0,0 +1,654 @@
+addMarker(
+    42.87328,
+    74.60356,
+    "Профессианальный Лицей № 3",
+    greenMarkerOptions,
+    greenMarkers
+  );
+  addMarker(
+    42.8713017,
+    74.6347285,
+    "Профессианальный Лицей № 17",
+    greenMarkerOptions,
+    greenMarkers
+  );
+  addMarker(
+    42.856633349999996,
+    74.5475596167581,
+    "Профессианальный Лицей № 97",
+    greenMarkerOptions,
+    greenMarkers
+  );
+  addMarker(
+    42.87531,
+    74.57389,
+    "Профессианальный Лицей № 10",
+    greenMarkerOptions,
+    greenMarkers
+  );
+  addMarker(
+    42.86259,
+    74.52132,
+    "Профессианальный Лицей № 27",
+    greenMarkerOptions,
+    greenMarkers
+  );
+  addMarker(
+    42.84805,
+    74.55515,
+    "Профессианальный Лицей № 91",
+    greenMarkerOptions,
+    greenMarkers
+  );
+  addMarker(
+    42.85403,
+    74.68038,
+    "Профессианальный Лицей № 18",
+    greenMarkerOptions,
+    greenMarkers
+  );
+  addMarker(
+    42.84953,
+    74.55585,
+    "Профессианальный Лицей № 20",
+    greenMarkerOptions,
+    greenMarkers
+  ); //ytyty
+  addMarker(
+    42.87833,
+    74.57282,
+    "Профессианальный Лицей № 100",
+    greenMarkerOptions,
+    greenMarkers
+  );
+  addMarker(
+    42.84985,
+    74.5552,
+    "Профессианальный Лицей № 94",
+    greenMarkerOptions,
+    greenMarkers
+  );
+  addMarker(
+    42.85995,
+    74.61019,
+    "Профессианальный Лицей № 5",
+    greenMarkerOptions,
+    greenMarkers
+  );
+  addMarker(
+    42.85562,
+    74.6299,
+    "Профессианальный Лицей № 99",
+    greenMarkerOptions,
+    greenMarkers
+  );
+  addMarker(
+    42.87111,
+    74.5863,
+    "Профессианальный Лицей № 4",
+    greenMarkerOptions,
+    greenMarkers
+  );
+  addMarker(
+    42.85653466344125,
+    74.67436016876017,
+    "Професссиальный Лицей № 93",
+    greenMarkerOptions,
+    greenMarkers
+  );
+  addMarker(
+    42.8769055835926,
+    74.5935563980311,
+    "Професссиальный Лицей № 98",
+    greenMarkerOptions,
+    greenMarkers
+  );
+  addMarker(
+    42.87054395203675,
+    74.58659353709334,
+    "КТЖПЛ",
+    greenMarkerOptions,
+    greenMarkers
+  );
+  //    chuy
+  addMarker(
+    43.10435,
+    74.3697,
+    "Профессианальный Лицей № 43",
+    greenMarkerOptions,
+    greenMarkers
+  );
+  addMarker(
+    42.8579,
+    74.28641,
+    "Профессианальный Лицей № 114",
+    greenMarkerOptions,
+    greenMarkers
+  );
+  addMarker(
+    42.80597,
+    73.82648,
+    "Профессианальный Лицей № 105",
+    greenMarkerOptions,
+    greenMarkers
+  );
+  addMarker(
+    42.81112,
+    75.44236,
+    "Профессианальный Лицей № 61",
+    greenMarkerOptions,
+    greenMarkers
+  );
+  addMarker(
+    42.73762,
+    74.54271,
+    "Профессианальный Лицей № 47",
+    greenMarkerOptions,
+    greenMarkers
+  );
+  addMarker(
+    42.88794,
+    74.86639,
+    "Профессианальный Лицей № 25",
+    greenMarkerOptions,
+    greenMarkers
+  );
+  addMarker(
+    42.82533,
+    74.11861,
+    "Профессианальный Лицей № 28",
+    greenMarkerOptions,
+    greenMarkers
+  );
+  addMarker(
+    42.7781,
+    75.7826,
+    "Профессианальный Лицей № 19",
+    greenMarkerOptions,
+    greenMarkers
+  );
+  addMarker(
+    42.81923,
+    73.87192,
+    "Профессианальный Лицей № 102",
+    greenMarkerOptions,
+    greenMarkers
+  );
+  addMarker(
+    42.96518,
+    74.78944,
+    "Профессианальный Лицей № 56",
+    greenMarkerOptions,
+    greenMarkers
+  );
+  addMarker(42.8328, 75.3027, "ТИППК", greenMarkerOptions, greenMarkers);
+  addMarker(
+    42.84931,
+    75.3091,
+    "Профессианальный Лицей № 23",
+    greenMarkerOptions,
+    greenMarkers
+  );
+  addMarker(
+    42.85584,
+    74.34286,
+    "Профессианальный Лицей № 106",
+    greenMarkerOptions,
+    greenMarkers
+  );
+  addMarker(
+    42.96353,
+    74.65723,
+    "Профессианальный Лицей № 37",
+    greenMarkerOptions,
+    greenMarkers
+  );
+  addMarker(
+    42.74439055186572,
+    75.01658139443673,
+    "Профессианальный Лицей № 49",
+    greenMarkerOptions,
+    greenMarkers
+  );
+  // osh
+  addMarker(
+    40.21933,
+    72.68517,
+    "Профессианальный Лицей № 54",
+    greenMarkerOptions,
+    greenMarkers
+  );
+  addMarker(
+    40.51772,
+    72.45114,
+    "Профессианальный Лицей № 112",
+    greenMarkerOptions,
+    greenMarkers
+  );
+  addMarker(
+    40.3084,
+    73.44946,
+    "Профессианальный Лицей № 67",
+    greenMarkerOptions,
+    greenMarkers
+  );
+  addMarker(
+    40.52538,
+    72.77255,
+    "Профессианальный Лицей № 12",
+    greenMarkerOptions,
+    greenMarkers
+  );
+  addMarker(
+    40.51922,
+    72.77495,
+    "Профессианальный Лицей № 113",
+    greenMarkerOptions,
+    greenMarkers
+  );
+  addMarker(
+    40.37666,
+    72.19792,
+    "Профессианальный Лицей № 65",
+    greenMarkerOptions,
+    greenMarkers
+  );
+  addMarker(
+    40.63127,
+    73.60772,
+    "Профессианальный Лицей № 29",
+    greenMarkerOptions,
+    greenMarkers
+  );
+  addMarker(
+    40.62072,
+    72.90519,
+    "Профессианальный Лицей № 59",
+    greenMarkerOptions,
+    greenMarkers
+  );
+  addMarker(
+    39.55245,
+    72.2019,
+    "Профессианальный Лицей № 44",
+    greenMarkerOptions,
+    greenMarkers
+  );
+  addMarker(
+    40.68501,
+    73.15334,
+    "Профессианальный Лицей № 62",
+    greenMarkerOptions,
+    greenMarkers
+  );
+  addMarker(
+    40.56803,
+    72.80332,
+    "Профессианальный Лицей № 16",
+    greenMarkerOptions,
+    greenMarkers
+  );
+  addMarker(
+    40.51693,
+    72.81443,
+    "Кыргызско-Узбекский профессиональный лицей",
+    greenMarkerOptions,
+    greenMarkers
+  );
+  addMarker(
+    40.24963,
+    72.54957,
+    "Профессианальный Лицей № 58",
+    greenMarkerOptions,
+    greenMarkers
+  );
+  addMarker(
+    40.30006825688623,
+    74.25230726911484,
+    "Профессианальный Лицей № 109", //tolko selo
+    greenMarkerOptions,
+    greenMarkers
+  );
+  // talas
+  addMarker(
+    42.67946,
+    71.30894,
+    "Профессианальный Лицей № 13",
+    greenMarkerOptions,
+    greenMarkers
+  );
+  addMarker(
+    42.5779,
+    71.82149,
+    "Профессианальный Лицей № 41",
+    greenMarkerOptions,
+    greenMarkers
+  );
+  addMarker(
+    42.54791,
+    72.21085,
+    "Профессианальный Лицей № 6",
+    greenMarkerOptions,
+    greenMarkers
+  );
+  addMarker(
+    42.48692,
+    72.52513,
+    "Профессианальный Лицей № 110",
+    greenMarkerOptions,
+    greenMarkers
+  );
+  addMarker(
+    42.76302,
+    71.44861,
+    "Профессианальный Лицей № 51",
+    greenMarkerOptions,
+    greenMarkers
+  );
+  addMarker(
+    42.52172,
+    72.25029,
+    "Профессианальный Лицей № 90",
+    greenMarkerOptions,
+    greenMarkers
+  );
+  // naryn
+  addMarker(
+    41.937843103955394,
+    74.54307279986767,
+    "Профессианальный Лицей № 26",
+    greenMarkerOptions,
+    greenMarkers
+  );
+  addMarker(
+    42.23355704637843,
+    75.74507332456092,
+    "Профессианальный Лицей № 15",
+    greenMarkerOptions,
+    greenMarkers
+  );
+  addMarker(
+    41.16898915351688,
+    75.81185861728176,
+    "Профессианальный Лицей № 32",
+    greenMarkerOptions,
+    greenMarkers
+  );
+  addMarker(
+    41.36106347016677,
+    75.526995895318,
+    "Профессианальный Лицей № 86",
+    greenMarkerOptions,
+    greenMarkers
+  );
+  addMarker(
+    41.26834279183164,
+    74.96068815520948,
+    "Профессианальный Лицей № 40",
+    greenMarkerOptions,
+    greenMarkers
+  );
+  addMarker(
+    41.428375914381256,
+    75.99122462453228,
+    "Профессианальный Лицей № 87",
+    greenMarkerOptions,
+    greenMarkers
+  );
+  addMarker(
+    41.44887662318898,
+    76.24088649754523,
+    "Профессианальный Лицей № 30",
+    greenMarkerOptions,
+    greenMarkers
+  );
+  // ИК
+  addMarker(
+    42.15345,
+    77.60118,
+    "Профессианальный Лицей № 55",
+    greenMarkerOptions,
+    greenMarkers
+  );
+  addMarker(
+    42.45828,
+    76.19328,
+    "Профессианальный Лицей № 22",
+    greenMarkerOptions,
+    greenMarkers
+  );
+  addMarker(
+    42.1175,
+    76.99203,
+    "Профессианальный Лицей № 81",
+    greenMarkerOptions,
+    greenMarkers
+  );
+  addMarker(
+    42.49882,
+    78.41011,
+    "Профессианальный Лицей № 14",
+    greenMarkerOptions,
+    greenMarkers
+  );
+  addMarker(
+    42.48139,
+    78.40444,
+    "Профессианальный Лицей № 2",
+    greenMarkerOptions,
+    greenMarkers
+  );
+  addMarker(
+    42.7341,
+    77.67616,
+    "Профессианальный Лицей № 82",
+    greenMarkerOptions,
+    greenMarkers
+  );
+  addMarker(
+    42.70255,
+    77.42259,
+    "Профессианальный Лицей № 42",
+    greenMarkerOptions,
+    greenMarkers
+  );
+  addMarker(
+    42.5376,
+    76.62265,
+    "Профессианальный Лицей № 85",
+    greenMarkerOptions,
+    greenMarkers
+  );
+  // db
+  addMarker(
+    41.68834414473658,
+    72.03154428992576,
+    "Профессианальный Лицей № 53",
+    greenMarkerOptions,
+    greenMarkers
+  );
+  addMarker(
+    41.49480038796203,
+    71.76633703740848,
+    "Профессианальный Лицей № 69",
+    greenMarkerOptions,
+    greenMarkers
+  );
+  addMarker(
+    41.6288122072342,
+    72.66672395522232,
+    "Профессианальный Лицей № 89",
+    greenMarkerOptions,
+    greenMarkers
+  );
+  addMarker(
+    41.0180031377509,
+    73.06090646642602,
+    "Профессианальный Лицей № 36",
+    greenMarkerOptions,
+    greenMarkers
+  );
+  addMarker(
+    40.94021671207776,
+    72.99813811960612,
+    "Профессианальный Лицей № 75",
+    greenMarkerOptions,
+    greenMarkers
+  );
+  addMarker(
+    40.91527951698383,
+    72.94378811273364,
+    "Профессианальный Лицей № 60",
+    greenMarkerOptions,
+    greenMarkers
+  );
+  addMarker(
+    41.77167884132286,
+    71.59833172228004,
+    "Профессианальный Лицей № 21",
+    greenMarkerOptions,
+    greenMarkers
+  );
+  addMarker(
+    41.41190680567737,
+    74.02278677705635,
+    "Профессианальный Лицей № 45",
+    greenMarkerOptions,
+    greenMarkers
+  );
+  addMarker(
+    41.8696120244212,
+    72.94276016710278,
+    "Профессианальный Лицей № 39",
+    greenMarkerOptions,
+    greenMarkers
+  );
+  addMarker(
+    41.20252806177205,
+    72.17741873336335,
+    "Профессианальный Лицей № 108",
+    greenMarkerOptions,
+    greenMarkers
+  );
+  addMarker(
+    41.04507839576438,
+    72.75150580711015,
+    "Профессианальный Лицей № 63",
+    greenMarkerOptions,
+    greenMarkers
+  );
+  addMarker(
+    40.99183526085072,
+    72.63987451106978,
+    "Профессианальный Лицей № 68",
+    greenMarkerOptions,
+    greenMarkers
+  );
+  addMarker(
+    41.048688525230155,
+    72.4823767853762,
+    "Профессианальный Лицей № 77",
+    greenMarkerOptions,
+    greenMarkers
+  );
+  addMarker(
+    41.33057100001628,
+    72.1969768841625,
+    "Профессианальный Лицей № 111",
+    greenMarkerOptions,
+    greenMarkers
+  );
+  addMarker(
+    41.768384852280484,
+    71.09490726403878,
+    "Профессианальный Лицей № 46",
+    greenMarkerOptions,
+    greenMarkers
+  );
+  addMarker(
+    40.94842520240765,
+    72.6203976696514,
+    "Профессианальный Лицей № 52",
+    greenMarkerOptions,
+    greenMarkers
+  );
+  // bat
+  addMarker(
+    40.2938890798925,
+    72.1288732419127,
+    "Профессианальный Лицей № 70",
+    greenMarkerOptions,
+    greenMarkers
+  );
+  addMarker(
+    39.835935687326874,
+    69.52785176865368,
+    "Профессианальный Лицей № 48",
+    greenMarkerOptions,
+    greenMarkers
+  );
+  addMarker(
+    40.24174324076383,
+    71.90535475301299,
+    "Профессианальный Лицей № 33",
+    greenMarkerOptions,
+    greenMarkers
+  );
+  addMarker(
+    39.9891830195538,
+    69.66721511554984,
+    "Профессианальный Лицей № 73",
+    greenMarkerOptions,
+    greenMarkers
+  );
+  addMarker(
+    40.29537670332773,
+    72.1901670562428,
+    "Профессианальный Лицей № 8",
+    greenMarkerOptions,
+    greenMarkers
+  );
+  addMarker(
+    40.168109338618976,
+    71.31141845517124,
+    "Профессианальный Лицей № 72",
+    greenMarkerOptions,
+    greenMarkers
+  );
+  addMarker(
+    40.00085688231172,
+    70.54443807050691,
+    "Профессианальный Лицей № 34",
+    greenMarkerOptions,
+    greenMarkers
+  );
+  addMarker(
+    40.05718273577688,
+    70.82680689735642,
+    "Профессианальный Лицей № 57",
+    greenMarkerOptions,
+    greenMarkers
+  );
+  addMarker(
+    40.24343775946205,
+    72.07250735517383,
+    "Профессианальный Лицей № 115",
+    greenMarkerOptions,
+    greenMarkers
+  );
+  addMarker(
+    40.089240850433505,
+    69.56993961085773,
+    "Профессианальный Лицей № 31",
+    greenMarkerOptions,
+    greenMarkers
+  );
+  addMarker(
+    40.26121781643314,
+    72.10646140914997,
+    "Профессианальный Лицей № 7",
+    greenMarkerOptions,
+    greenMarkers
+  );
+  //
+  // маркер лицеев конец
